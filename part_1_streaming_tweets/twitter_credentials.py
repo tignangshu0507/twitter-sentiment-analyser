e02d10276b2a542f1b0bcac0ab202940bcc03bfe
Tignangshu Chatterjee <48378764+tignangshu0507@users.noreply.github.com>
2020-06-06 21:52:28 +0530

    Add files via upload

---
+++ part_1_streaming_tweets/twitter_credentials.py
@@ -0,0 +1,6 @@
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "1113114787473371136-LkVk3f9WUNwLQpPAtGEKNUiLIaNUKy"
+ACCESS_TOKEN_SECRET = "bc00YOL8RNML16NfaIXuvtI5H03g1W5zHYdFziARd74kJ"
+CONSUMER_KEY = "BJYwL1Fxt2iMsnjwOK4wmZ8UM"
+CONSUMER_SECRET = "Pwz722rig9yYKcIP1DE8kZnvoNvogwqAWm7266DzYfNKOmqNMV"
+#please do change the value of the variables after you have created a twitter app copy your own credentials to the variables 
